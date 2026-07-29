0e80ccea0f299a1c0c9df9526b3ee9e86fc47bf6
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-28 21:30:54 -0300

    Feito

---
+++ src/app/calculadora/calculadora.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormsModule } from
+'@angular/forms';
+
+@Component({
+selector: 'app-calculadora',
+standalone: true,
+imports: [FormsModule],
+templateUrl:
+'./calculadora.html',
+styleUrl:
+'./calculadora.css'
+})
+
+export class Calculadora {
+numero1 = 0;
+
+numero2 = 0;
+
+resultado = 0;
+
+somar(){
+
+  this.resultado = this.numero1 + this.numero2
+}
+
+subtrair(){
+
+  this.resultado = this.numero1 - this.numero2
+}
+
+multiplicar(){
+
+  this.resultado = this.numero1 * this.numero2
+}
+
+dividir(){
+
+  this.resultado = this.numero1 / this.numero2
+}
+
+
+}
